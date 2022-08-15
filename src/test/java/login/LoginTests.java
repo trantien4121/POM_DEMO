43a2adc9c7bece8b262e6eaa570a43b3cb753ba7
TranTien4121 <trantien04012k1@gmail.com>
2022-08-15 10:31:04 +0700

    updated

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -9,7 +9,7 @@ import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends BaseTests{
     @Test
-    public void testSuccessfullyLogin(){
+    public void testSuccessfullyLogin() {
         LoginPage loginPage = homePage.goToLogin();
         loginPage.login("huyquoc@gmail.com", "12345678");
         assertTrue(loginPage.getSuccessTitle().contains("Welcome to Safe Railway"), "Login Failed!!");
